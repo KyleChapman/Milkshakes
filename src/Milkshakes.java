6d6de2500b59a719fe81a02bbdea8bfb85c22ab2
KyleChapman <kyle.chapman@durhamcollege.ca>
2026-05-27 12:03:48 -0400

    Cleanup - mostly adding comments for content explanation.

--- src/Milkshakes.java
+++ src/Milkshakes.java
@@ -1,5 +1,6 @@
 // Author:  Kyle Chapman
-// Created: May 25, 2025
+// Created: May 25, 2026
+// Updated: May 26, 2026
 // Description: Polls the user about different types of milkshakes
 // and uses that input to determine which milkshakes are the most
 // popular.
@@ -13,25 +14,38 @@ public class Milkshakes {
     public static void main(String[] args) {
 
         // Declarations.
+        // Quantities of milkshakes.
         int vanillaShakes = 0;
         int chocolateShakes = 0;
         int camelMilkShakes = 0;
+        // Variables for percentages, used in the processing near the end of the program.
+        // We could argue about whether these should be declared later:
+        // See https://google.github.io/styleguide/javaguide.html#s4.8.2-variable-declarations .
         double vanillaPercent;
         double chocolatePercent;
         double camelMilkPercent;
+        // A total number of shakes, used in division and to check for divide-by-zero.
         int totalShakes;
+        // A boolean variable used to check for valid input.
         boolean isValid = false;
+        // Represents the user's menu choice.
         String choice;
 
         // Input.
+        // Use a do..while loop to force the user to enter an option at least once.
+        // This loop continues until option 4 is selected.
         do {
+            // Print a menu.
             System.out.println("1. Vanilla");
             System.out.println("2. Chocolate");
             System.out.println("3. Camel Milk");
             System.out.println("4. Tally and Exit");
             System.out.println("Please enter a menu choice (1-4): ");
 
+            // Get a menu choice as a string. The strip() method is used to remove whitespace.
             choice = input.nextLine().strip();
+            // Assume any further input is invalid.
+            isValid = false;
 
             switch (choice) {
                 case "1":
@@ -49,7 +63,6 @@ public class Milkshakes {
                     break;
                 case "2":
                     // Chocolate milkshake input.
-                    isValid = false;
                     while (!isValid) {
                         System.out.print("Enter the number of chocolate shakes sold: ");
                         if (input.hasNextInt()) {
@@ -63,7 +76,6 @@ public class Milkshakes {
                     break;
                 case "3":
                     // Camel milk milkshakes.
-                    isValid = false;
                     while (!isValid) {
                         System.out.print("Enter the number of camel milk shakes sold: ");
                         if (input.hasNextInt()) {
@@ -88,9 +100,11 @@ public class Milkshakes {
 
         // Processing.
 
-        // For comparison, the comments are the processing from Python (without indentation)!
-        // total_milk = vanilla + chocolate + strawberry
+        // Calculating a total makes the math a lot cleaner/shorter soon.
         totalShakes = vanillaShakes + chocolateShakes + camelMilkShakes;
+
+        // This part is a check for a total of zero, since dividing by zero breaks math.
+        // For comparison, these comments are the processing from Python (without indentation)!
         // if total_milk == 0:
         if (totalShakes == 0) {
             // print("No Milkshakes Sold :(")
@@ -114,10 +128,10 @@ public class Milkshakes {
             // print("Strawberry Milkshakes: " + str(strawberry) + " or " + str(round(strawberry_percent,1))+ "%")
             System.out.printf("Camel Milk Milkshakes: %d or %.2f%%%n", camelMilkShakes, camelMilkPercent);
             // print("My Milkshake brings all the Camels to the yard!")
-            System.out.println("Ishaan is silly.");
+            System.out.println("Thanks Ishaan.");
         }
 
-        // Confirm close.
+        // Confirm close. Some people would insist on this.
         System.out.print("\nPress Enter to end the program... ");
         // Wait for the user to press Enter.
         input.nextLine();
